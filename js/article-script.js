f5a1be2e6372a2c2a7f4cd416743d13927dd59be
angelaslinker <angela.r.slinker@gmail.com>
2023-10-26 17:53:16 -0700

    added grid to content

---
+++ js/article-script.js
@@ -0,0 +1,46 @@
+// Fetch the JSON data from your file (assuming the JSON file is named data.json)
+fetch('./json/articles.json')
+    .then(response => response.json())
+    .then(data => {
+        const mondayAssets = data[0].monday_assets; // Assuming you want the first item in the JSON array
+
+        // Access specific images by their keys
+        const celebritiesImage1 = mondayAssets[0].celebrities_image1;
+        const celebritiesImage2 = mondayAssets[1].celebrities_image2;
+        const celebritiesImage3 = mondayAssets[2].celebrities_image3;
+        const celebritiesImage4 = mondayAssets[3].celebrities_image4;
+        const celebritiesImage5 = mondayAssets[4].celebrities_image5;
+        const celebritiesImage6 = mondayAssets[5].celebrities_image6;
+        const celebritiesImage7 = mondayAssets[6].celebrities_image7;
+        const celebritiesImage8 = mondayAssets[7].celebrities_image8;
+
+
+
+        // Select the existing image elements by their class (assuming all images have the same class "image")
+        const imageElements = document.querySelectorAll('.image');
+
+        // Set the 'src' attributes for the images
+        imageElements[0].src = celebritiesImage1;
+        imageElements[1].src = celebritiesImage2;
+        imageElements[2].src = celebritiesImage3;
+        imageElements[3].src = celebritiesImage4;
+        imageElements[4].src = celebritiesImage5;
+        imageElements[5].src = celebritiesImage6;
+        imageElements[6].src = celebritiesImage7;
+        imageElements[7].src = celebritiesImage8;
+
+
+
+        // Access the 'Monday' title from the JSON data
+        const mondayTitle = data[0].monday_articles;
+
+        // Find the HTML element with the class "title" and update its content
+        const titleElement = document.querySelector('.title');
+        titleElement.textContent = mondayTitle[0].celebrities_title;
+        // Find the HTML element with the class "article" and update its content
+        const articleElement = document.querySelector('.article');
+        articleElement.textContent = mondayTitle[0].celebrities_article;
+    })
+    .catch(error => {
+        console.error('Error fetching JSON data:', error);
+    });
